67d43c2b82793df87a116e0d91e022b9863102f0
Sihyeon Hong <72115242+sihyun-mity@users.noreply.github.com>
2023-05-30 23:38:16 +0900

    Initial global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/ Home';
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="community-dragon-downloader/*">
+          <Route path="" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ src/pages/Home/ Home.tsx
@@ -0,0 +1,8 @@
+import React, { ReactElement } from 'react';
+import { HomeContainer } from './home.styles';
+
+const Home = (): ReactElement => {
+  return <HomeContainer>Hi</HomeContainer>;
+};
+
+export default Home;
